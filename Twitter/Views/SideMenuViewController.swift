47db2e6c4b2c239e104658aae6de6204012fd71d
hina-oikawa <kingu741113@gmail.com>
2019-10-11 17:47:23 +0900

    サイドメニューのViewを追加

---
+++ Twitter/Views/SideMenuViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SideMenuViewController.swift
+//  Twitter
+//
+//  Created by 及川ひな on 2019/10/11.
+//  Copyright © 2019 oikawa. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.isHidden = true
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
